97a9f62ec7b9598ab43a4833515ab81796f6a7e5
Jonathan Tabeling <jptabe@yahoo.com>
2026-05-15 06:34:33 -0400

    ecosystem again

--- yjs-server/ecosystem.config.cjs
+++ yjs-server/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: 'yjs-server',
         script: './server.js',
-        cwd: '/home/isidore/scriptorium/yjs-server',
+        cwd: '/var/www/scriptorium/yjs-server',
         env: {
             PORT: 1889,
             HOST: '0.0.0.0',
